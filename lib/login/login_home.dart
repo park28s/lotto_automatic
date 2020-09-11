cbc776bea33feb4513f1676a3051ba0d1583c18b
park28s <park28s@naver.com>
2020-09-11 13:21:51 +0900

    Lotto-20.09.11

---
+++ lib/login/login_home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginHome extends StatefulWidget {
+  @override
+  _LoginHomeState createState() => _LoginHomeState();
+}
+
+class _LoginHomeState extends State<LoginHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
