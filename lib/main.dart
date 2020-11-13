e0f00b6ef3d5c8a5ff1c473603c61e93fa90e5cc
DESKTOP-IP09S5L\Administrator <park28s@naver.com>
2020-11-13 15:48:54 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lotto_app/main/main_home.dart';
-import
-
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
